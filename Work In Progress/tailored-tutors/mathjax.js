199cb7843da363018657bdaf1e66c51932c7dfb2
Seppe Verhavert <seppe.verhavert@gmail.com>
2020-08-04 17:19:00 +0200

    finish mathjax JS

--- Work In Progress/tailored-tutors/mathjax.js
+++ Work In Progress/tailored-tutors/mathjax.js
@@ -1,40 +1,28 @@
+window.MathJax = {
+    tex: {
+        tags: 'ams',
+        displayMath: [
+            ['$$', '$$'],
+            ['\\(', '\\)']
+        ],
+        inlineMath: [
+            ['$', '$'],
+            ['\\(', '\\)']
+        ],
+        autoload: ['verb', 'color']
+    }
+};
+
 var script = document.createElement('script');
 
-App.Slides.registerEvent('rsOnUpdateNav', function() {
+script.id = "MathJax-script";
+script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+script.onload = function() {
+    MathJax.config.startup.ready();
 
-    script.onload = function() {
-        window.MathJax = {
-            tex: {
-                tags: 'ams',
-                inlineMath: [
-                    ['$', '$'],
-                    ['\\(', '\\)']
-                ],
-                autoload: ['verb', 'color']
-            }
-        };
-    };
-
-    script.id = "MathJax-script";
-    script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
-
-    $(window).on('load', function() {
-        console.log("load");
-        if (debug) console.log('%c on load ', 'color: green; font-weight: bold;', successUltimate);
-        checkComments();
-        setTimeout(function() {
-            MathJax.config.startup.ready();
-        }, 1000);
+    App.Slides.registerEvent('rsOnUpdateNav', function() {
+        MathJax.typeset()
     });
-
-    $(window).on('lecture:change', function() {
-        console.log("lecture:change");
-        if (debug) console.log('%c lecture:change ', 'color: green; font-weight: bold;', successUltimate);
-        serveComments();
-        setTimeout(function() {
-            MathJax.config.startup.ready();
-        }, 1000);
-    });
-});
+};
 
 document.head.appendChild(script);
