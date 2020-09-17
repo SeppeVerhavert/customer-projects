bc1a9a007786e245ec5e9ea61353ea62931580b6
Seppe Verhavert <seppe.verhavert@gmail.com>
2020-09-17 11:22:28 +0200

    move to old

---
+++ Done or Hold/projects/09-2020/capgemini/script.js
@@ -0,0 +1,18 @@
+//      -----   CUSTOM JS SCRIPT     -----       //
+
+require(['underscore'], function(_) {
+
+    //      -----   FUNCTIONS ON SURVEYLOAD     -----       //
+
+    App.functions.onSurveyLoaded(function(slidesView) {
+
+        //      -----   CHANGE FOOTER TO HYPERLINK     -----       //
+
+        slidesView.collection.each(function() {
+
+            let $newFooter = $('<div class="footer-item branding"><a href="https://s.surveyanyplace.com/vniercpp" target="_blank"><span>Personal data</span></a></div>');
+            App.applicationView.$el.find('.footer-item').remove();
+            App.applicationView.$el.find('.footer').prepend($newFooter);
+        });
+    });
+})
