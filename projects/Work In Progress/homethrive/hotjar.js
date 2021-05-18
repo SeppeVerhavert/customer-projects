5e18f8c7e409fd6c2817fd5396fb1999dcf7dacf
SeppeVerhavert <seppe.verhavert@gmail.com>
2021-05-18 17:52:13 +0200

    add script for homethrive

---
+++ projects/Work In Progress/homethrive/hotjar.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    App.functions.onSurveyLoaded(function() {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.innerHTML = "(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:2404503,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');";
+        document.head.appendChild(script);
+    });
+});
