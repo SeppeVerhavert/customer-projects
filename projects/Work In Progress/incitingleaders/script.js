c758cba3a70298cfea9d7f6d28bae70c71fac4f7
SeppeVerhavert <seppe.verhavert@gmail.com>
2022-12-09 11:30:16 +0100

    add files for incitingleaders

---
+++ projects/Work In Progress/incitingleaders/script.js
@@ -0,0 +1,31 @@
+docReady(function() {
+    var group_labels = document.querySelectorAll('#Group_Percentage_Labels .data-group .category-labels #gtext tspan');
+    var value_labels = document.querySelectorAll('#Values_Labels .data-group .category-labels #gtext tspan');
+
+    for (var i = 0; i < group_labels.length; i++) {
+        var score = group_labels[i].innerHTML.slice(0, (group_labels[i].innerHTML.length - 1));
+        group_labels[i].innerHTML = (Math.round((score / 20) * 100) / 100);
+    }
+    for (var j = 0; j < value_labels.length; j++) {
+        var score = value_labels[j].innerHTML
+        value_labels[j].innerHTML = (Math.round((score / 20) * 100) / 100);
+    }
+});
+
+
+function docReady(fn) {
+    // see if DOM is already available
+    console.log("pretimeout");
+
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+
+    } else {
+        document.addEventListener("DOMContentLoaded", fn, false);
+
+        window.addEventListener('load', (event) => {
+            console.log('DOM fully loaded and parsed');
+        });
+    }
+}
